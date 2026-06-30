28c6bf1d4ccbf28dabd57c5a14254d17777925cd
msctop4 <150358456+msctop4@users.noreply.github.com>
2026-06-30 23:30:27 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,12 +1,6 @@
+# start.py
+
 import asyncio
-from src.bot import client
-from src.inlinebot.bot import bot, dp
+from src.bot import main
 
-async def main():
-    await client.start()
-    await asyncio.gather(
-        client.run_until_disconnected(), 
-        dp.start_polling(bot))
-
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
